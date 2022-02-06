50c1b857bfd0f74b63cb87b9d0664622559c13f5
markus <me@markuskonrad.de>
2022-02-06 22:18:21 +0100

    Initial commit with gen and config file and README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MK.CertificateGenerator
+
 A tool creating Certificate Authority and Server Certificate using OpenSSL.
+
+Was tested February 2022 with iOS Version 15.2.1, macOS 11.2.2 and Windows 10.
+
+Kudos to [TechSchool](<https://www.youtube.com/c/TECHSCHOOLGURU/about>). Their video [Create & sign SSL/TLS certificates with openssl](<https://youtu.be/7YgaZIFn7mY>) was used as a source to generate this tool.
+
+## Usage
+
---
+++ src/gen.sh
@@ -0,0 +1,26 @@
+# Remove existing .pem and .srl files in the current folder.
+rm *.pem
+rm *.srl
+
+# 1. Generate CA´s private key and self-signed certificated
+# - The 'nodes' option avoids password (for dev and internal use only and required if used in Synology)
+# - The 'days' option must be less or equal to 825 to support Apple´s 'Requirements for trusted certificates' (https://support.apple.com/en-us/HT210176)
+# - The 'sha256' value is important and must be of family 2xx. SHA1 is not supported by most Operating Systems anymore
+# - Change the over values to fit to your needs
+openssl req -x509 -newkey rsa:4096 -days 700 -sha256 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=DE/ST=YOURCOUNTRY/L=YOURCITY/O=YOURCANAME/OU=HQ/CN=*.YOURCADOMAIN.com/emailAddress=YOU@YOURAUTHORITY.com"
+echo "CA´s self-signed certificate:"
+openssl x509 -in ca-cert.pem -noout -text # Subject for next command can be fetched from here
+
+# 2. Generate web server´s private key and signing request (CSR)
+# - No days value here, since this is not a Cert but a Certificate Signing Request
+openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=DE/ST=YOURCOUNTRY/L=YOURCITY/O=YOURSERVERNAME/OU=YOUROU/CN=*.YOURSERVERDOMAIN.com/emailAddress=INFO@YOURSERVERDOMAIN.com"
+
+# 3. Use CA´s private key to sign web server´s CSR and get back the signed certificate
+# - Including also SAN information via configuration file 'server-ext.cnf'
+openssl x509 -req -days 700 -sha256 -in server-req.pem -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
+echo "Server´s self-signed certificate:"
+openssl x509 -in server-cert.pem -noout -text
+
+# 4. Verify Certificate
+echo "Verify is Cert is valid:"
+openssl verify -CAfile ca-cert.pem server-cert.pem
